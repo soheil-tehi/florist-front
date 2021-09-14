99f3a05a0e21c0317c8203a8be0d158087381b1e
tehrani <tehrani@gmail.com>
2021-09-14 16:07:23 +0430

    remove font google

--- src/pages/PlantSelect/HousePlants.js
+++ src/pages/PlantSelect/HousePlants.js
@@ -64,9 +64,7 @@ const HousePlants = () => {
                       className="img-fluid"
                       alt=""
                     />
-                    {
-                      console.log("kaktos.images[0]",kaktos.images[0])
-                    }
+                
                     <div className="portfolio-links">
                       <h4>{kaktos.name}</h4>
                       {/* <a
